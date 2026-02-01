6269216603156e7f9d6e714b32862a441e4ff377
Christian Yip <christianyippers@gmail.com>
2026-02-01 02:49:27 -0800

    Home Page Buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ export default function Home() {
     <main className="min-h-screen bg-black">
       <div className="container mx-auto px-4 py-16">
         <div className="text-center mb-16">
-          <h1 className="font-[zuume] text-9xl font-bold italic text-white mb-4">
+          <h1 className="font-[zuume] text-8xl font-bold italic text-white">
             BCHL Officiating
           </h1>
-          <p className="text-2xl text-gray-400 font-bold uppercase tracking-wider">
+          <p className=" text-white font-bold uppercase tracking-wider">
             Tracking Excellence on the Ice
           </p>
         </div>
-        <div>
-          <a href="/team">Our Team</a>
+        <div className="flex flex-row gap-4">
+          <a href="/team" className="px-4 py-2 font-black uppercase text-sm transition-colors duration-300
+          bg-white text-black hover:bg-orange-600 hover:text-white
+          ">Learn More</a>
+          <a href="/team" className="px-4 py-2 font-black uppercase text-sm transition-colors duration-300
+          bg-transparent text-white border-2 border-white hover:bg-orange-600 hover:border-orange-600
+          ">Our Team</a>
         </div>
       </div>
     </main>
